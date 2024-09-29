1e570e0250d7a67a36d9c88289e0d34fd22b04a4
Artem Babii <arthem.babij@gmail.com>
2024-09-29 10:47:08 +0300

    add gnome 47 support

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
 import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
-import * as Config from 'resource:///org/gnome/shell/misc/config.js';
+import * as Config from "resource:///org/gnome/shell/misc/config.js";
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
 import St from "gi://St";
@@ -11,161 +11,178 @@ const ICON = "utilities-terminal-symbolic";
 const BULLET = "pan-end-symbolic";
 
 const ScrollableMenu = class ScrollableMenu extends PopupMenu.PopupMenuSection {
-    constructor() {
-        super();
-        const scrollView = new St.ScrollView();
-        this.innerMenu = new PopupMenu.PopupMenuSection();
-        const shellVersion = parseFloat(Config.PACKAGE_VERSION).toString().slice(0, 2);
-        if (shellVersion == 45) {
-            scrollView.add_actor(this.innerMenu.actor);
-            this.actor.add_actor(scrollView);
-        } else {
-            scrollView.add_child(this.innerMenu.actor);
-            this.actor.add_child(scrollView);
-        }
+  constructor() {
+    super();
+    const scrollView = new St.ScrollView();
+    this.innerMenu = new PopupMenu.PopupMenuSection();
+    const shellVersion = parseFloat(Config.PACKAGE_VERSION)
+      .toString()
+      .slice(0, 2);
+    if (shellVersion == 45) {
+      scrollView.add_actor(this.innerMenu.actor);
+      this.actor.add_actor(scrollView);
+    } else {
+      scrollView.add_child(this.innerMenu.actor);
+      this.actor.add_child(scrollView);
     }
+  }
 };
 
 export default class LauncherExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._indicator = null;
-        this._menuId = null;
-        this._settings = null;
-        this._launcher = null;
-        this._menu = null;
-        this._path = null;
+  constructor(metadata) {
+    super(metadata);
+    this._indicator = null;
+    this._menuId = null;
+    this._settings = null;
+    this._launcher = null;
+    this._menu = null;
+    this._path = null;
+  }
+
+  _appendLog(script, stdout, stderr) {
+    const logName = `${GLib.get_home_dir()}/.${this.metadata.name}.log`;
+    const logFile = Gio.File.new_for_path(logName);
+    const encoder = new TextEncoder();
+    const date = new Date();
+
+    const outputStream = logFile.append_to(Gio.FileCreateFlags.NONE, null);
+    outputStream.write(encoder.encode(`\n[${script}]: ${date}\n`), null);
+    outputStream.write(encoder.encode(`STDOUT:\n${stdout}`), null);
+    outputStream.write(encoder.encode(`STDERR:\n${stderr}`), null);
+    outputStream.close(null);
+  }
+
+  _fillMenu() {
+    this._menu.innerMenu.removeAll();
+
+    this._path = this._settings.get_string("path");
+    if (!this._path) {
+      return;
     }
 
-    _appendLog(script, stdout, stderr) {
-        const logName = `${GLib.get_home_dir()}/.${this.metadata.name}.log`;
-        const logFile = Gio.File.new_for_path(logName);
-        const encoder = new TextEncoder();
-        const date = new Date();
+    const shebangIcon = this._settings.get_boolean("shebang-icon");
+    const dafaultIcon = this._settings.get_string("default-icon");
+    const stripExt = this._settings.get_boolean("strip");
 
-        const outputStream = logFile.append_to(Gio.FileCreateFlags.NONE, null);
-        outputStream.write(encoder.encode(`\n[${script}]: ${date}\n`), null);
-        outputStream.write(encoder.encode(`STDOUT:\n${stdout}`), null);
-        outputStream.write(encoder.encode(`STDERR:\n${stderr}`), null);
-        outputStream.close(null);
+    this._getScripts(this._path).forEach((script) => {
+      this._menu.innerMenu.addAction(
+        stripExt
+          ? script.get_name().replace(/\.[^\.]+$/, "")
+          : script.get_name(),
+        () => this._launchScript(script.get_name()),
+        shebangIcon ? script.get_icon() : dafaultIcon || BULLET,
+      );
+    });
+  }
+
+  _getScripts(path) {
+    const directory = Gio.File.new_for_path(path);
+    if (!directory.query_exists(null)) {
+      return;
     }
 
-    _fillMenu() {
-        this._menu.innerMenu.removeAll();
+    const enumerator = directory.enumerate_children(
+      "standard::name,standard::type,standard::icon",
+      Gio.FileQueryInfoFlags.NONE,
+      null,
+    );
+    const scripts = [];
 
-        this._path = this._settings.get_string("path");
-        if (!this._path) {
-            return;
-        }
+    while (true) {
+      const fileInfo = enumerator.next_file(null);
+      if (!fileInfo) {
+        break;
+      }
 
-        const shebangIcon = this._settings.get_boolean("shebang-icon");
-        const dafaultIcon = this._settings.get_string("default-icon");
-        const stripExt = this._settings.get_boolean("strip");
+      const fileType = fileInfo.get_file_type();
+      if (fileType === Gio.FileType.REGULAR) {
+        scripts.push(fileInfo);
+      }
+    }
 
-        this._getScripts(this._path).forEach((script) => {
-            this._menu.innerMenu.addAction(
-                stripExt ? script.get_name().replace(/\.[^\.]+$/, "") : script.get_name(),
-                () => this._launchScript(script.get_name()),
-                shebangIcon ? script.get_icon() : dafaultIcon || BULLET
+    enumerator.close(null);
+    scripts.sort((a, b) => a.get_name().localeCompare(b.get_name()));
+    return scripts;
+  }
+
+  _launchScript(script) {
+    this._indicator.menu.toggle();
+    const command = [`${this._path}/${script}`];
+
+    try {
+      const proc = this._launcher.spawnv(command);
+      proc.communicate_utf8_async(null, null, (proc, res) => {
+        const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+
+        const notify = this._settings.get_boolean("notify");
+        if (notify) {
+          if (stdout || stderr) {
+            Main.notify(this.metadata.name, `[${script}]: ${stdout || stderr}`);
+          } else {
+            Main.notify(
+              this.metadata.name,
+              `[${script}]: completed with exit code: ${proc.get_exit_status()}`,
             );
-        });
-    }
-
-    _getScripts(path) {
-        const directory = Gio.File.new_for_path(path);
-        if (!directory.query_exists(null)) {
-            return;
+          }
         }
 
-        const enumerator = directory.enumerate_children(
-            "standard::name,standard::type,standard::icon",
-            Gio.FileQueryInfoFlags.NONE,
-            null
-        );
-        const scripts = [];
-
-        while (true) {
-            const fileInfo = enumerator.next_file(null);
-            if (!fileInfo) {
-                break;
-            }
-
-            const fileType = fileInfo.get_file_type();
-            if (fileType === Gio.FileType.REGULAR) {
-                scripts.push(fileInfo);
-            }
+        const logging = this._settings.get_boolean("log");
+        if (logging) {
+          this._appendLog(script, stdout, stderr);
         }
-
-        enumerator.close(null);
-        scripts.sort((a, b) => a.get_name().localeCompare(b.get_name()));
-        return scripts;
+      });
+    } catch (e) {
+      Main.notify(
+        this.metadata.name,
+        `[${script}]: ${e.toString().replace("GLib.SpawnError: ", "")}`,
+      );
     }
+  }
 
-    _launchScript(script) {
-        this._indicator.menu.toggle();
-        const command = [`${this._path}/${script}`];
+  _addIndicator() {
+    this._indicator = new PanelMenu.Button(0.5, this.metadata.name, false);
 
-        try {
-            const proc = this._launcher.spawnv(command);
-            proc.communicate_utf8_async(null, null, (proc, res) => {
-                const [, stdout, stderr] = proc.communicate_utf8_finish(res);
+    const icon = new St.Icon({
+      gicon: new Gio.ThemedIcon({ name: ICON }),
+      style_class: "system-status-icon",
+    });
+    this._indicator.add_child(icon);
 
-                const notify = this._settings.get_boolean("notify");
-                if (notify) {
-                    if (stdout || stderr) {
-                        Main.notify(this.metadata.name, `[${script}]: ${stdout || stderr}`);
-                    } else {
-                        Main.notify(this.metadata.name, `[${script}]: completed with exit code: ${proc.get_exit_status()}`);
-                    }
-                }
+    this._menu = new ScrollableMenu();
+    this._indicator.menu.addMenuItem(this._menu);
+    this._indicator.menu.addAction(
+      "Settings",
+      () => this.openPreferences(),
+      "preferences-system-symbolic",
+    );
 
-                const logging = this._settings.get_boolean("log");
-                if (logging) {
-                    this._appendLog(script, stdout, stderr);
-                }
-            });
-        } catch (e) {
-            Main.notify(this.metadata.name, `[${script}]: ${e.toString().replace("GLib.SpawnError: ", "")}`);
+    Main.panel.addToStatusArea(this.metadata.name, this._indicator);
+
+    this._menuId = this._indicator.menu.connect(
+      "open-state-changed",
+      (open) => {
+        if (open) {
+          this._fillMenu();
         }
-    }
+      },
+    );
+  }
 
-    _addIndicator() {
-        this._indicator = new PanelMenu.Button(0.5, this.metadata.name, false);
+  enable() {
+    this._addIndicator();
+    this._settings = this.getSettings();
+    this._launcher = new Gio.SubprocessLauncher({
+      flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+    });
+  }
 
-        const icon = new St.Icon({
-            gicon: new Gio.ThemedIcon({ name: ICON }),
-            style_class: "popup-menu-icon",
-        });
-        this._indicator.add_child(icon);
-
-        this._menu = new ScrollableMenu();
-        this._indicator.menu.addMenuItem(this._menu);
-        this._indicator.menu.addAction("Settings", () => this.openPreferences(), "preferences-system-symbolic");
-
-        Main.panel.addToStatusArea(this.metadata.name, this._indicator);
-
-        this._menuId = this._indicator.menu.connect("open-state-changed", (open) => {
-            if (open) {
-                this._fillMenu();
-            }
-        });
-    }
-
-    enable() {
-        this._addIndicator();
-        this._settings = this.getSettings();
-        this._launcher = new Gio.SubprocessLauncher({
-            flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
-        });
-    }
-
-    disable() {
-        this._indicator.menu.disconnect(this._menuId);
-        this._indicator.destroy();
-        this._indicator = null;
-        this._menuId = null;
-        this._menu = null;
-        this._settings = null;
-        this._launcher = null;
-    }
+  disable() {
+    this._indicator.menu.disconnect(this._menuId);
+    this._indicator.destroy();
+    this._indicator = null;
+    this._menuId = null;
+    this._menu = null;
+    this._settings = null;
+    this._launcher = null;
+  }
 }
--- prefs.js
+++ prefs.js
@@ -5,119 +5,140 @@ import GLib from "gi://GLib";
 import Gtk from "gi://Gtk";
 
 export default class LauncherPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
+  fillPreferencesWindow(window) {
+    const settings = this.getSettings();
 
-        const page = new Adw.PreferencesPage();
-        const group = new Adw.PreferencesGroup();
-        page.add(group);
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup();
+    page.add(group);
 
-        // Enter Path
-        const rowPath = new Adw.ActionRow({
-            title: "Enter Path",
-            subtitle: "Directory with your scripts",
-        });
-        group.add(rowPath);
+    // Enter Path
+    const rowPath = new Adw.ActionRow({
+      title: "Enter Path",
+      subtitle: "Directory with your scripts",
+    });
+    group.add(rowPath);
 
-        const entryPath = new Gtk.Entry({
-            placeholder_text: "/home/username/myscripts",
-            text: settings.get_string("path"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const entryPath = new Gtk.Entry({
+      placeholder_text: "/home/username/myscripts",
+      text: settings.get_string("path"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("path", entryPath, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("path", entryPath, "text", Gio.SettingsBindFlags.DEFAULT);
 
-        rowPath.add_suffix(entryPath);
-        rowPath.activatable_widget = entryPath;
+    rowPath.add_suffix(entryPath);
+    rowPath.activatable_widget = entryPath;
 
-        // Log
-        const rowLog = new Adw.ActionRow({
-            title: "Log",
-            subtitle: `${GLib.get_home_dir()}/.${this.metadata.name}.log`,
-        });
-        group.add(rowLog);
+    // Log
+    const rowLog = new Adw.ActionRow({
+      title: "Log",
+      subtitle: `${GLib.get_home_dir()}/.${this.metadata.name}.log`,
+    });
+    group.add(rowLog);
 
-        const toggleLog = new Gtk.Switch({
-            active: settings.get_boolean("log"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleLog = new Gtk.Switch({
+      active: settings.get_boolean("log"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("log", toggleLog, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("log", toggleLog, "active", Gio.SettingsBindFlags.DEFAULT);
 
-        rowLog.add_suffix(toggleLog);
-        rowLog.activatable_widget = toggleLog;
+    rowLog.add_suffix(toggleLog);
+    rowLog.activatable_widget = toggleLog;
 
-        // Notify
-        const rowNotify = new Adw.ActionRow({
-            title: "Notify",
-            subtitle: "Show a notification (stdout || stderr || exit code) on script completion",
-        });
-        group.add(rowNotify);
+    // Notify
+    const rowNotify = new Adw.ActionRow({
+      title: "Notify",
+      subtitle:
+        "Show a notification (stdout || stderr || exit code) on script completion",
+    });
+    group.add(rowNotify);
 
-        const toggleNotify = new Gtk.Switch({
-            active: settings.get_boolean("notify"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleNotify = new Gtk.Switch({
+      active: settings.get_boolean("notify"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("notify", toggleNotify, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "notify",
+      toggleNotify,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowNotify.add_suffix(toggleNotify);
-        rowNotify.activatable_widget = toggleNotify;
+    rowNotify.add_suffix(toggleNotify);
+    rowNotify.activatable_widget = toggleNotify;
 
-        // Shebang Icon
-        const rowIconType = new Adw.ActionRow({
-            title: "Shebang Icon",
-            subtitle: "Use script shebang to set an icon",
-        });
-        group.add(rowIconType);
+    // Shebang Icon
+    const rowIconType = new Adw.ActionRow({
+      title: "Shebang Icon",
+      subtitle: "Use script shebang to set an icon",
+    });
+    group.add(rowIconType);
 
-        const toggleIconType = new Gtk.Switch({
-            active: settings.get_boolean("shebang-icon"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleIconType = new Gtk.Switch({
+      active: settings.get_boolean("shebang-icon"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("shebang-icon", toggleIconType, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "shebang-icon",
+      toggleIconType,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowIconType.add_suffix(toggleIconType);
-        rowIconType.activatable_widget = toggleIconType;
+    rowIconType.add_suffix(toggleIconType);
+    rowIconType.activatable_widget = toggleIconType;
 
-        // Default Icon
-        const rowIconName = new Adw.ActionRow({
-            title: "Default Icon",
-            subtitle: "Used when shebang icon is disabled",
-        });
-        group.add(rowIconName);
+    // Default Icon
+    const rowIconName = new Adw.ActionRow({
+      title: "Default Icon",
+      subtitle: "Used when shebang icon is disabled",
+    });
+    group.add(rowIconName);
 
-        const entryIconName = new Gtk.Entry({
-            placeholder_text: "pan-end-symbolic",
-            text: settings.get_string("default-icon"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const entryIconName = new Gtk.Entry({
+      placeholder_text: "pan-end-symbolic",
+      text: settings.get_string("default-icon"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("default-icon", entryIconName, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "default-icon",
+      entryIconName,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowIconName.add_suffix(entryIconName);
-        rowIconName.activatable_widget = entryIconName;
+    rowIconName.add_suffix(entryIconName);
+    rowIconName.activatable_widget = entryIconName;
 
-        // Strip
-        const rowStrip = new Adw.ActionRow({
-            title: "Strip",
-            subtitle: "Hide file extensions",
-        });
-        group.add(rowStrip);
+    // Strip
+    const rowStrip = new Adw.ActionRow({
+      title: "Strip",
+      subtitle: "Hide file extensions",
+    });
+    group.add(rowStrip);
 
-        const toggleStrip = new Gtk.Switch({
-            active: settings.get_boolean("strip"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleStrip = new Gtk.Switch({
+      active: settings.get_boolean("strip"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("strip", toggleStrip, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "strip",
+      toggleStrip,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowStrip.add_suffix(toggleStrip);
-        rowStrip.activatable_widget = toggleStrip;
+    rowStrip.add_suffix(toggleStrip);
+    rowStrip.activatable_widget = toggleStrip;
 
-        window.add(page);
-    }
+    window.add(page);
+  }
 }
